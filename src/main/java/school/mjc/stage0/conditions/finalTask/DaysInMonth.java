01969716bee4ab6db572c4de5e6609728f645522
Alex Myachin <myachin_aleksei@mail.ru>
2022-08-25 18:00:00 +0600

    done

--- src/main/java/school/mjc/stage0/conditions/finalTask/DaysInMonth.java
+++ src/main/java/school/mjc/stage0/conditions/finalTask/DaysInMonth.java
@@ -2,6 +2,24 @@ package school.mjc.stage0.conditions.finalTask;
 
 public class DaysInMonth {
     public void printDays(int year, int month) {
-
+        if (year > 0) {
+            switch (month) {
+                case 1, 3, 5, 7, 8, 10, 12:
+                    System.out.println(31);
+                    break;
+                case 4, 6, 9, 11:
+                    System.out.println(30);
+                    break;
+                case 2:
+                    month = year % 4 == 0 ? year % 100 == 0 ? year % 400 == 0 ? 29 : 28 : 29 : 28;
+                    System.out.println(month);
+                    break;
+                default:
+                    System.out.println("invalid date");
+                    break;
+            }
+        } else {
+            System.out.println("invalid date");
+        }
     }
 }
